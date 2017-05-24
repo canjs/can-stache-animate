db11b0d4f85b01df6d9bb39001d57a55f786bef2
Mick McGrath <mick.mc.13@gmail.com>
2017-05-24 13:54:22 -0500

    #9 - beforeremove demo & implementation proposal

---
+++ src/can-util/dom/mutate/mutate.js
@@ -0,0 +1,12 @@
+var domMutate = require("can-util/dom/mutate/mutate");
+var domDispatchAsync = require("can-stache-animate/can-util/dom/dispatch-async/dispatch-async");
+
+var oldRemoveChild = domMutate.removeChild;
+domMutate.removeChild = function(child){
+	var self = this;
+	domDispatchAsync.call(child,{
+		type: "beforeremove"
+	}, [], false).then(function(){
+		oldRemoveChild.call(self, child);
+	});
+};
